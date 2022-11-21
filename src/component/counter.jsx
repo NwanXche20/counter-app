e87d34c6d0c2b0c2a7b50d7513a395e1099e8747
Nwaneri Uchechukwu Chigozie <58370329+NwanXche20@users.noreply.github.com>
2022-11-21 10:00:17 +0000

    Changed the text in the buttons

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
             onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
           >
-            +
+            Increment
           </button>
 
           <button
@@ -20,14 +20,14 @@ class Counter extends Component {
             className="btn btn-secondary btn-sm m-2"
             disabled={this.props.counter.value === 0}
           >
-            -
+            Decrement
           </button>
 
           <button
             className="btn btn-sm badge-danger m-2"
             onClick={() => this.props.onDelete(this.props.counter.id)}
           >
-            x
+            Del
           </button>
         </div>
       </div>
